b31305fa2f565422a2d9b14a85de1678303d44a0
johnl <johnlyle@dnamicro.com>
2024-07-30 20:47:16 +0800

    plane and cell wave animation

--- utils/cell-loop.js
+++ utils/cell-loop.js
@@ -4,24 +4,24 @@ document.querySelector('a-scene').addEventListener('loaded', () => {
   const delay1 = 1000
   const delay2 = 500
   for (let j = 0; j < 8; j++) {
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 40; i++) {
       const modelEntity = document.createElement('a-entity');
       modelEntity.setAttribute('gltf-model', '#Cell');
-      const cellX = i * 0.500
-      const cellY = j * -0.400
-      const initialPos = `${cellX} 1.100 ${cellY}`;
+      const cellX = i * 1.5
+      const cellZ = j * -1.5
+      const initialPos = `${cellX} 1.100 ${cellZ}`;
       modelEntity.setAttribute('position', initialPos);
-      modelEntity.setAttribute('scale', '0.300 0.300 0.300');
+      modelEntity.setAttribute('scale', '1 1 1');
       
       const animationAttr = `
         property: position;
         from: ${initialPos};
-        to: ${cellX} 1 ${cellY};
+        to: ${cellX} 1.5 ${cellZ};
         loop: true;
-        dur: 1000;
+        dur: 900;
         dir: alternate;
-        elasticity: 0;
-        delay: ${i%2 === 0 ? delay1 :  delay2};
+        // elasticity: 0;
+        delay: ${i * 100};
       `;
       modelEntity.setAttribute('animation', animationAttr);
 
