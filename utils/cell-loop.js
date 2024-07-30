72fe4938f93c9aeb600e5c2371e678ac757329e4
johnl <johnlyle@dnamicro.com>
2024-07-30 20:33:26 +0800

    animated cells

--- utils/cell-loop.js
+++ utils/cell-loop.js
@@ -1,13 +1,31 @@
 document.querySelector('a-scene').addEventListener('loaded', () => {
   const container = document.querySelector('#cell');
 
-  for (let i = 0; i < 100; i++) {
-    const modelEntity = document.createElement('a-entity');
-    modelEntity.setAttribute('gltf-model', '#Cell');
-    const initialPos = `${i * 0.400} 1.472 0`
-    modelEntity.setAttribute('position', initialPos);
-    modelEntity.setAttribute('scale', `0.300 0.300 0.300`);
-    modelEntity.setAttribute('animation', `property: position; from:${initialPos}; to: ${i * 0.400} 1 0; loop: true; dur: 1000; dir: alternate; elasticity: 0; delay: ${Math.floor(Math.random() * 1000)}`);
-    container.appendChild(modelEntity);
+  const delay1 = 1000
+  const delay2 = 500
+  for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 50; i++) {
+      const modelEntity = document.createElement('a-entity');
+      modelEntity.setAttribute('gltf-model', '#Cell');
+      const cellX = i * 0.500
+      const cellY = j * -0.400
+      const initialPos = `${cellX} 1.100 ${cellY}`;
+      modelEntity.setAttribute('position', initialPos);
+      modelEntity.setAttribute('scale', '0.300 0.300 0.300');
+      
+      const animationAttr = `
+        property: position;
+        from: ${initialPos};
+        to: ${cellX} 1 ${cellY};
+        loop: true;
+        dur: 1000;
+        dir: alternate;
+        elasticity: 0;
+        delay: ${i%2 === 0 ? delay1 :  delay2};
+      `;
+      modelEntity.setAttribute('animation', animationAttr);
+
+      container.appendChild(modelEntity);
+    }
   }
 });
